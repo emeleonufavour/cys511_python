215faf5147b4705d457a617f34a1bb41566f9902
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-01-19 19:57:12 +0100

    updated password cracking algo

--- firstname.py
+++ firstname.py
@@ -14,46 +14,101 @@ fnames = {
         "Asegun", "Kolade", "Anu", "Anuoluwa", "Mary", "Aisha", "Chinenye", "Chris", "Christopher", "Favour",
         "Kasarachi", "Abisoye", "Taiye", "Edward", "Ayotunde", "Mofi", "Mofifunoluwa"
     },
-    # 4: {},
-    # 5: {},
-    # 6: {},
-    # 7: {},
-    # 8: {},
-    # 9: {},
-    # 10: {},
-    # 11: {},
-    # 12: {},
-    # 13: {},
-    # 14: {},
-    # 15: {},
-    # 16: {},
-    # 17: {},
-    # 18: {},
-    # 19: {},
-    # 20: {},
-    # 21: {},
+    4: {},
+    5: {},
+    6: {},
+    7: {},
+    8: {},
+    9: {},
+    10: {},
+    11: {},
+    12: {},
+    13: {},
+    14: {},
+    15: {},
+    16: {},
+    17: {},
+    18: {},
+    19: {},
+    20: {},
+    21: {},
 }
 
 fnames2 = {
-    2: {
-        "Mark", "Lois", "Kingsley", "Olusegun", "Favour", "Mofiyinfoluwa", "Chika", "Michael", "David", "Isaac",
-        "Chukwunonso", "Divine", "Mamoke", "Isaiah", "Marvellous"
-    },
+    # 2: {
+    #     "Mark", "Lois", "Kingsley", "Olusegun", "Favour", "Mofiyinfoluwa", "Chika", "Michael", "David", "Isaac",
+    #     "Chukwunonso", "Divine", "Mamoke", "Isaiah", "Marvellous"
+    # },
     3: {
-        "Nnaemeka", "Samuel", "Iyioluwa", "David", "Okikiola", "Asegun", "Kolade", "Anuoluwa", "Aisha", "Chinenye",
-        "Christopher", "Kasarachi", "Abisoye", "Taiye", "Ayotunde", "Mofifunoluwa"
+        # "Petu-Ibikunle"
+        # "Nnaemeka", "Samuel", "Iyioluwa", "David", "Okikiola", "Asegun", "Kolade", "Anuoluwa", "Aisha", "Chinenye",
+        # "Christopher", "Kasarachi", "Abisoye", "Taiye", "Ayotunde", "Mofifunoluwa"
     },
     4: {
-        "Theodore", "Toluwani", "Terdoo", "Tomide", "Ayomide", "Davina", "Praise", "Fredrick", "Jude", "Samuel",
-        "Praise", "Toluwanimi", "Oluwamayomikun", "Ebenezer", "Favour"
+        "Oluwamayomikun",
+        # "Theodore", "Toluwani", "Terdoo", "Tomide", "Ayomide", "Davina", "Praise", "Fredrick", "Jude", "Samuel",
+        # "Praise", "Toluwanimi",  "Ebenezer", "Favour",
     },
     5: {
-        "Rex", "Praise", "Ajibola", "Daniel", "Ibikunoluwa", "Israel", "Moses", "Theresa", "Purity", "Dominion",
+        "Rex", "Praise", "Ajibola", "Daniel", "Ibukunoluwa", "Israel", "Moses", "Theresa", "Purity", "Dominion",
         "Oluwajuwonlo", "Afolabi", "David", "Divine", "Oluwaseyanu"
     },
-    6: {
-        "Victor", "Abiodun", "Ogheneyoma", "Daniel", "Ololade", "Ololade", "Idris", "Emmanuel", "Naomi", "Faustina",
-        "Mathew", "Oghenemaro", "Mmesoma", "Abraham", "Praise"
-    }
-
+    # 6: {
+    #     "Victor", "Abiodun", "Ogheneyoma", "Daniel", "Ololade", "Idris", "Emmanuel", "Naomi", "Faustina",
+    #     "Mathew", "Oghenemaro", "Mmesoma", "Abraham", "Praise", "David"
+    # },
+    # 7: {
+    #     "Samuel", "Iyanuoluwa", "Samson", "Blessing", "Daniel", "Victor", "Joshua", "Oseremen", "Osaruonamen", "Ejiro",
+    #     "Davidson", "Victor", "Obaloluwa", "Kyomnom", "Kayode"
+    # },
+    8: {
+        "Confidence", "Clement", "David", "Divine", "Onaopemipo", "Oluwaseyitan", "Favour", "Israel", "David",
+        "Promise", "Ribetnan", "Oluwaseyi", "Victor", "Kojusoluwa", "Temiloluwa"
+    },
+    9: {
+        "Faith", "Oluwapemi", "Emmanuel", "Joshua", "Daniel", "Emmanuel", "Gem", "Imoudu", "Jedidiah", "Ese", "Great",
+        "Feranmi", "Isaac", "Chiamaka", "Gregory"
+    },
+    10: {
+        "Tennyson", "Oluwalonimi", "Oklo", "Joshua", "Daniel", "Ayomide", "Ayomide", "Ade", "Ayomide Ade",
+        "Toluwanimi", "Babajide",
+        "Paul", "David",
+        "Nkem", "Goodness", "Tifeoluwa", "Mayowa"
+    },
+    11: {
+        "Ayebatonte", "Bamigboluwaga", "Blessing", "Chinenye", "Samuel", "Oluseun", "Motolani", "Samuel", "Emmanuel",
+        "Omonuwa", "Wandoo", "Goodness", "Preye", "Ebenezer", "Nancy", "Nafisat",
+    },
+    12: {
+        "Daniel", "Theodore", "Iyinoluwa", "Michael", "Toluwalope", "Emmanuel", "David", "Joy", "Tobiloba", "Precious",
+        "Chidubem", "Ireoluwa", "Janet", "Samuel"
+    },
+    # 13: {
+    #     "Daniel", "Joseph", "Daniel", "Chidozie", "Joy", "Victor", "Ibukunoluwa", "David-Paul", "Timilehin", "Horo",
+    #     "Zachariah", "Hazel", "Toluwalase", "Chinemerem", "Chisom", "Jesse", "Paul", "Jesse Paul"
+    # },
+    14: {
+        "Temiloluwa", "Jedidiah", "Ayooluwa", "Marvellous", "Lumi", "Ibukun", "Victor", "Akinolu", "Adeoluwa",
+        "Oghenebrorhien", "Divine", "Ayooluwatomiwa", "Daniel", "Netaniah", "Victor"
+    },
+    15: {
+        "Fiyinfoluwa", "Paradise", "Asher", "Sobirat", "Samuel", "Oluwadamilola", "Onyekachi", "Victor", "Clinton",
+        "Camsyochi", "John", "Ifeoluwa", "Owenpraise", "Gideon", "Elizabeth", "Wisdom"
+    },
+    16: {"Angel", "Ayooluwa", "Chukwuemeka", "Tobore", "Oluwabori", "David", "Ayodele", "Damola", "Olalekan",
+         "Oluwatobi",
+         "Seyi", "Joseph", "Precious", "Favour", "Ejiroghene", },
+    # 17: {
+    #     "Emmanuel", "Peter", "Amanda", "Tolulope", "Ehinomen", "Oluwanifemi", "Ebubechukwu", "Valentine", "Tamilore",
+    #      "Justina", "Samuel", "Faith", "Delight", "Olatunde", "Israel",
+    # },
+    18: {"Wisdom", "David", "Etinosa", "Samuel", "Oluwatimilehin", "Prince", "Olumide", "Oluwatitomi", "Chuku",
+         "Oghenekeno", "Tomiwa", "Hannah", "Victor", "Peter", "Emmanuel", },
+    19: {"Glory", "Collins", "Samuel", "Tirenioluwa", "Grace", "Victor", "Isaac", "Jesse", "Joy", "Dominion", "Adeseye",
+         "Olamide", "Michael", "Ori-Ogan", "Uzoma", },
+    20: {"Emmanuel", "Adedamola", "Ayodeji", "Oluwatayo", "Jonathan", "David", "Damilare", "Oluwatobi", "Oluwanifemi",
+         "Emmanuel", "Melody", "Gbohunmi", "Olaoluwakitan", "Oyinkansola", "Oluwabunmi", },
+    21: {"Prince", "Temiloluwa", "David", "Daniel", "Joesph", "Telimoye", "Emmanuel", "Damilola", "Bernard",
+         "Chukwuemeka", "Joshua", "Mosopefoluwa", "Oluwatosin", "Ayodeji", "Justice", "Oluwatimilehin", "Esther",
+         "Kenechukwu", }
 }
--- password_crack.py
+++ password_crack.py
@@ -1,13 +1,38 @@
 import hashlib
 import os
+import string
 from itertools import permutations
 from first_names import fnames, fnames2
-from passlib.hash import htdigest
+from passlib.hash import htdigest, sha512_crypt
 from passlib.hash import pbkdf2_sha256, md5_crypt
 
 shadow_file = open('/Users/mac/Downloads/cys_test/shadow.txt')
 hash_to_password = {}
 visited_groups = {}
+
+
+# Creating a list from username, salt and password
+def extract_info():
+    f = open('/Users/mac/Downloads/cys_test/shadow.txt', "r")
+    lines = f.read().split()
+    f.close()
+
+    # extract username, salts, hashes into lists
+    usernames = []
+    salts = []
+    hashes = []
+
+    for line in lines:
+        if "$6" in line:
+            # clean out the entries into expected part
+            sections = line.split("$")
+            # clean out the entries into expected part
+            usernames.append(sections[0].split(":")[0])
+            # clean out the entries into expected part
+            salts.append(sections[2])
+            # clean out the entries into expected part
+            hashes.append(sections[3].split(":")[0])
+    return usernames, salts, hashes
 
 
 def read_shadow_file():
@@ -81,7 +106,7 @@ class PasswordCracker:
                     elements[0], elements[n - 1] = elements[n - 1], elements[0]
             self.generate_permutations(n - 1, elements, result)
 
-    def your_password(self, no, username, password_hash):
+    def your_password(self, no, username, salt, password_hash):
         if (password_hash in hash_to_password) or (no in visited_groups):
             # print(f"{hash_to_password[password_hash]} is the reveal of {password_hash}")
             return
@@ -89,81 +114,71 @@ class PasswordCracker:
             combinations = self.possible_combinations()
             for combination in combinations:
                 permutations_list = []
-                self.generate_permutations(len(combination), combination, permutations_list)
-                possible_passwords = [''.join(map(str, e)) for e in permutations_list]
+                self.generate_permutations(
+                    len(combination), combination, permutations_list)
+                possible_passwords = [''.join(map(str, e))
+                                      for e in permutations_list]
 
                 for password in possible_passwords:
-                    bytes_ = password.encode('utf-8')
-                    bytes_lower = password.lower().encode('utf-8')
-                    bytes_upper = password.upper().encode('utf-8')
+                    # bytes_ = password.encode('utf-8')
+                    # bytes_lower = password.lower().encode('utf-8')
+                    # bytes_upper = password.upper().encode('utf-8')
 
-                    hash_algo = identify_hash_algorithm(password_hash)
-
-                    # MD5 hashing
-                    if hash_algo == "MD5":
-                        result = hashlib.md5(bytes_).hexdigest()
-                        result_lower = hashlib.md5(bytes_lower).hexdigest()
-                        result_upper = hashlib.md5(bytes_upper).hexdigest()
-
-                    # SHA-1 hashing
-                    elif hash_algo == "SHA-1":
-                        result = hashlib.sha1(bytes_).hexdigest()
-                        result_lower = hashlib.sha1(bytes_lower).hexdigest()
-                        result_upper = hashlib.sha1(bytes_upper).hexdigest()
-
-                    # SHA-256 hashing
-                    elif hash_algo == "SHA-256":
-                        result = hashlib.sha256(bytes_).hexdigest()
-                        result_lower = hashlib.sha256(bytes_lower).hexdigest()
-                        result_upper = hashlib.sha256(bytes_upper).hexdigest()
-
-                    # SHA-384 hashing
-                    # result = hashlib.sha384(bytes_).hexdigest()
-                    # result_lower = hashlib.sha384(bytes_lower).hexdigest()
-
-                    # SHA-512 hashing
-                    else:
-                        result = hashlib.sha512(bytes_).hexdigest()
-                        result_lower = hashlib.sha512(bytes_lower).hexdigest()
-                        result_upper = hashlib.sha512(bytes_upper).hexdigest()
+                    result = sha512_crypt.using(rounds=5000, salt=str(
+                        salt)).hash(str(password)).split("$")[-1]
+                    result_lower = sha512_crypt.using(rounds=5000, salt=str(
+                        salt)).hash(str(password).lower()).split("$")[-1]
+                    result_upper = sha512_crypt.using(rounds=5000, salt=str(
+                        salt)).hash(str(password).upper()).split("$")[-1]
 
                     if result == password_hash:
                         hash_to_password[password_hash] = password
                         visited_groups[no] = True
-                        print(f"{no}: My guess for {username} password is {password}")
+                        print(
+                            "===========================================================")
+                        print(
+                            f"Group {no}: My guess for {username} password is {password}")
+                        print(
+                            "===========================================================")
                         return
+                    # else:
+                    #     print(f"{password} is not it")
 
                     if result_lower == password_hash:
                         hash_to_password[password_hash] = password
                         visited_groups[no] = True
-                        print(f"{no}: My guess for {username} password is {password.lower()}")
+                        print(
+                            "====================================================================")
+                        print(
+                            f"Group {no}: My guess for {username} password is {password.lower()}")
+                        print(
+                            "====================================================================")
                         return
 
                     if result_upper == password_hash:
                         hash_to_password[password_hash] = password
                         visited_groups[no] = True
-                        print(f"{no}: My guess for {username} password is {password.lower()}")
+                        print(
+                            "===========================================================")
+                        print(
+                            f"Group {no}: My guess for {username} password is {password.lower()}")
+                        print(
+                            "===========================================================")
                         return
 
 
 # Example Usage
-special_characters = "!@#$%^&*()-=_+[]{}|;:'\",.<>?/£§"
+# special_characters = "!@#$%^&*()-=_+[]{}|;:'\",.<>?/£§"
+special_characters = string.punctuation
 months = list(range(1, 13))
 days = list(range(1, 32))
 my_password = "10@Mofi13"
-
-password_cracker = PasswordCracker(
-    first_names=fnames[1],  # Replace with actual first names
-    days=days,
-    months=months,
-    special_characters=special_characters
-)
 
 bytes_ = my_password.encode('utf-8')
 sha512_result = hashlib.sha512(bytes_).hexdigest()
 
 # .............................................................
-# dummy_hash = "163d6dc7e05504f091c7dec094c18893"
+# dummy_hash = "29WHaLyz0mwwQkMQkfFejUgZ1JqlyNL2SymkJrmJeT2PzW66zUEuc1g/B7i2gy9Iqgak8UOc16xCIG3.6wXiN/"
 # hash_algo = identify_hash_algorithm(dummy_hash)
 
 # print(f"The identified algorithm is: {algorithm}")
@@ -172,8 +187,19 @@ sha512_result = hashlib.sha512(bytes_).hexdigest()
 
 
 username_to_pass = read_shadow_file()
+(usernames, salts, hashes) = extract_info()
 
-for username, hashed_password in username_to_pass.items():
+# for username, hashed_password in username_to_pass.items():
+#     for group_no, names in fnames2.items():
+#         password_cracker = PasswordCracker(
+#             first_names=names,
+#             days=days,
+#             months=months,
+#             special_characters=special_characters
+#         )
+#         print(f"In Group {group_no}: Finding a password to similar hash of: {hashed_password}...")
+#         password_cracker.your_password(group_no, username, hashed_password)
+for i in range(len(hashes)):
     for group_no, names in fnames2.items():
         password_cracker = PasswordCracker(
             first_names=names,
@@ -181,5 +207,10 @@ for username, hashed_password in username_to_pass.items():
             months=months,
             special_characters=special_characters
         )
-        print(f"In Group {group_no}: Finding a password to similar hash of: {hashed_password}...")
-        password_cracker.your_password(group_no, username, hashed_password)
+        print(
+            f"In Group {group_no} - {names}: Finding a password of {usernames[i]}")
+        print(f"With salt {salts[i]} to similar hash of: {hashes[i]}...")
+        password_cracker.your_password(
+            group_no, usernames[i], salts[i], hashes[i])
+# my_hash = sha512_crypt.using(rounds=5000, salt="eNgEGA1iz9ALVchi").hash("11%tijesunimi17").split("$")[-1]
+# print(my_hash)
