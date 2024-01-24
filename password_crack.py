541a7f1d41dd10f6902127a29ff4c74d1b59ac35
thefixer <emeleonufavour15@gmail.com>
2024-01-24 23:10:01 +0100

    Update password_crack.py

--- password_crack.py
+++ password_crack.py
@@ -50,29 +50,6 @@ def read_shadow_file():
     return user_password_map
 
 
-def identify_hash_algorithm(hash_string):
-    hash_length = len(hash_string)
-
-    # Check hash length to narrow down possibilities
-    if hash_length == 32:
-        return "MD5"
-    elif hash_length == 40:
-        return "SHA-1"
-    elif hash_length == 64:
-        return "SHA-256"
-    elif hash_length == 128:
-        return "SHA-512"
-
-    # Check for common prefixes or signatures
-    if hash_string.startswith("$pbkdf2-sha256$"):
-        return "PBKDF2-SHA256"
-
-    # Add more checks as needed for other algorithms
-
-    # If no match is found
-    return "Unknown Algorithm"
-
-
 class PasswordCracker:
     def __init__(self, first_names, days, months, special_characters):
         self.first_names = first_names
@@ -93,7 +70,7 @@ class PasswordCracker:
 
         return all_combinations
 
-    # Fastest for generating permutations
+   
     def generate_permutations(self, n, elements, result):
         if n == 1:
             result.append(elements.copy())
@@ -108,7 +85,7 @@ class PasswordCracker:
 
     def your_password(self, no, username, salt, password_hash):
         if (password_hash in hash_to_password) or (no in visited_groups):
-            # print(f"{hash_to_password[password_hash]} is the reveal of {password_hash}")
+            
             return
         else:
             combinations = self.possible_combinations()
@@ -120,9 +97,7 @@ class PasswordCracker:
                                       for e in permutations_list]
 
                 for password in possible_passwords:
-                    # bytes_ = password.encode('utf-8')
-                    # bytes_lower = password.lower().encode('utf-8')
-                    # bytes_upper = password.upper().encode('utf-8')
+                   
 
                     result = sha512_crypt.using(rounds=5000, salt=str(
                         salt)).hash(str(password)).split("$")[-1]
@@ -140,8 +115,7 @@ class PasswordCracker:
                         print(
                             "===========================================================")
                         return
-                    # else:
-                    #     print(f"{password} is not it")
+                  
 
                     if result_lower == password_hash:
                         hash_to_password[password_hash] = password
@@ -155,40 +129,17 @@ class PasswordCracker:
                         return
 
 
-# Example Usage
 # special_characters = "!@#$%^&*()-=_+[]{}|;:'\",.<>?/£§"
 special_characters = string.punctuation
 days = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18",
         "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
 months = ["01", "02", "03", "04", "05",
           "06", "07", "08", "09", "10", "11", "12"]
-my_password = "10@Mofi13"
-
-bytes_ = my_password.encode('utf-8')
-sha512_result = hashlib.sha512(bytes_).hexdigest()
-
-# .............................................................
-# dummy_hash = "29WHaLyz0mwwQkMQkfFejUgZ1JqlyNL2SymkJrmJeT2PzW66zUEuc1g/B7i2gy9Iqgak8UOc16xCIG3.6wXiN/"
-# hash_algo = identify_hash_algorithm(dummy_hash)
-
-# print(f"The identified algorithm is: {algorithm}")
-# print(f"The hash algorithm used ==> {hash_algo}")
-# .............................................................
 
 
 username_to_pass = read_shadow_file()
 (usernames, salts, hashes) = extract_info()
 
-# for username, hashed_password in username_to_pass.items():
-#     for group_no, names in fnames2.items():
-#         password_cracker = PasswordCracker(
-#             first_names=names,
-#             days=days,
-#             months=months,
-#             special_characters=special_characters
-#         )
-#         print(f"In Group {group_no}: Finding a password to similar hash of: {hashed_password}...")
-#         password_cracker.your_password(group_no, username, hashed_password)
 for i in range(len(hashes)):
     for group_no, names in fnames2.items():
         password_cracker = PasswordCracker(
