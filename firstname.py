5afe972781a9ec56d1af17c02a2999f0f02d6512
thefixer <emeleonufavour15@gmail.com>
2024-01-24 23:10:52 +0100

    Update firstname.py

--- firstname.py
+++ firstname.py
@@ -1,38 +1,3 @@
-fnames = {
-    1: {
-        "Oluwasefunmi", "Daniel", "Funmi", "Ayobolaji", "Bolaji", "Jason", "Israel", "Emmanuel", "Tijesunimi",
-        "Enoch", "Tomiwa", "Ayooluwatomiwa", "Ayodele", "Dele", "Victor", "Joyce", "Kehinde", "Taiye", "Favour",
-        "Oluwasanmi", "Sanmi", "Treasure", "Isaac", "Gideon",
-    },
-    2: {
-        "Mark", "Junior", "Lois", "Moji", "Mojirayo", "Kingsley", "Segun", "Olusegun", "Favour", "Mofi",
-        "Mofiyinfoluwa", "Chika", "Peter", "Michael", "David", "Isaac", "Nonso", "Chukwunonso", "Divine", "Samuel",
-        "Mamoke", "Isaiah", "Marvellous", "Ayomide",
-    },
-    3: {
-        "Emmanuel", "Samuel", "Nnaemeka", "Emeka", "Iyi", "Iyioluwa", "Peace", "David", "Gbenga", "Okikiola", "Adoye",
-        "Asegun", "Kolade", "Anu", "Anuoluwa", "Mary", "Aisha", "Chinenye", "Chris", "Christopher", "Favour",
-        "Kasarachi", "Abisoye", "Taiye", "Edward", "Ayotunde", "Mofi", "Mofifunoluwa"
-    },
-    4: {},
-    5: {},
-    6: {},
-    7: {},
-    8: {},
-    9: {},
-    10: {},
-    11: {},
-    12: {},
-    13: {},
-    14: {},
-    15: {},
-    16: {},
-    17: {},
-    18: {},
-    19: {},
-    20: {},
-    21: {},
-}
 
 fnames2 = {
     # 2: {
